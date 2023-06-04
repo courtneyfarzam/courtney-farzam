b2bb8dbdf2167c5d217ce83271a9a3cd19b485aa
cmash93 <cmash93@gmail.com>
2023-06-03 19:13:48 -0500

    added schemas to sanity. backend set up

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import about from './about';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import projects from './projects';
+import skills from './skills';
+import testimonials from './testimonials';
+import workExperience from './workExperience';
+
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experiences,
+    projects,
+    skills,
+    testimonials,
+    workExperience
+];
