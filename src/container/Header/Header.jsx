d4d0de790c5bfdb52c39814f17074f9303cfd5f4
cmash93 <cmash93@gmail.com>
2023-06-18 17:03:00 -0500

    css changes...bg color, primary, secondary

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => (
             transition={{ duration: 0.5, delayChildren: 0.5 }}
             className='app__header-img'
         >
-            <img src={images.profile} alt='profile-bg' />
+            <img src={images.profile} alt='profile-bg' className='profile-bg' />
 
             <motion.img
                 whileInView={{ scale: [0, 1] }}
